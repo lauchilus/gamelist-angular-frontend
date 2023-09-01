c1485f8957ae331e3ef0fc958b10e088789b396c
lautaro falfan <lautarofalfan@gmail.com>
2023-09-01 17:46:28 -0300

    modifiyng images functionality

--- src/app/common/playing.ts
+++ src/app/common/playing.ts
@@ -3,7 +3,7 @@ export class Playing {
     constructor(
         public id: number,
         public name: string,
-        public description: string,
+        public summary: string,
         public image: string,
         public startDate: Date,
         public finishDate: Date,
